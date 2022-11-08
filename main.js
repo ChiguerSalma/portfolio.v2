21fd70427fb7560fcfda62470151a56de867d656
ChiguerSalma <chiguersalma01@gmail.com>
2022-11-08 12:39:56 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function openDetails(evt, location) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(location).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+AOS.init();
+  
